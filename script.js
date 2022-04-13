33f593f468be6d62a7dab998b72333879a3b8da1
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2022-04-13 16:11:46 +0545

    Added functionalities

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 let apiQuotes=[];
+const quoteContainer=document.getElementById("quote-container");
+const quoteContent=document.getElementById("quote");
+const authorContent=document.getElementById("author");
+const twitterButton=document.getElementById("twitter");
+const nextQuoteButton=document.getElementById("new-quote");
+
 
 function changeQuote(quoteText){
-    document.getElementById("quote").innerHTML=quoteText;
+    quoteContent.textContent=quoteText;
 }
 
 function changeAuthor(authorName){
-    document.getElementById("author").innerHTML=authorName;
+   authorContent.textContent=authorName;
 }
 
-//aget Quotes from API
+//new Quotes
+function newQuote(apiQuotes){
+    let quote=apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+
+         if(quote.text.length>65){
+             quoteContainer.classList.add('long-quote');
+             changeQuote(quote.text);
+         }else{
+            changeQuote(quote.text);
+        }
+   
+        !quote.author?  changeAuthor("Unknown"):  changeAuthor(quote.author);
+}
+
+//get Quotes from API
 const getQuotes=async ()=>{
-    const apiUrl="https://type.fit/api/quotes"; 
+     const apiUrl="https://type.fit/api/quotes"; 
     const response= await fetch(apiUrl);
-
     const apiQuotes=await response.json();
-    let randomNumber=Math.floor(Math.random()*apiQuotes.length);
-    changeQuote(apiQuotes[randomNumber].text);
-    changeAuthor(apiQuotes[randomNumber].author);
+    newQuote(apiQuotes);
 }
 
+//IIFE
+//to fill up the quote when the page loades
+(getQuotes());
 
-document.getElementById("new-quote").addEventListener("click",getQuotes);
 
+//Twweet Quote
+function tweetQuote(){
+    let tweetUrl=`https://twitter.com/intent/tweet?text=${quoteContent.text} - ${authorContent}`;
+    window.open(tweetUrl,'_blank');
+}
 
-// //On load
-// getQuotes()
-// .then(data=>{
-//     // data.forEach((data)=>console.log(data.text));
-//     console.log(data[0].text);
-// });
+//Adding event listener
+nextQuoteButton.addEventListener('click',getQuotes);
+twitterButton.addEventListener('click',tweetQuote);
